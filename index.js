000fcfa2aa9be99b92519d6930cacf507f883de5
sittora <sittora@gmail.com>
2021-08-15 21:43:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+function findMatching(collection, name) {
+    const result = collection.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    return result;
+}
+
+
+function fuzzyMatch(drivers, letters){
+    const result = drivers.filter(driver => driver.startsWith(letters));
+    return result;
+}
+
+
+function matchName(drivers, name) {
+    const result = drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+    return result;
+}
